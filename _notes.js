684ddaac3f970fac90124aef863d5404f10b02e2
omarwebdeve <omarf977865@gmail.com>
2022-10-13 12:42:05 +0600

    gmail auth

--- _notes.js
+++ _notes.js
@@ -6,4 +6,21 @@
  * instal firebase npm install firebase
  * confige firebase apps
  * export config file
+ * create auth using getAuth from firedase by using app frim fitrbase.init.js
+ * create a google auth provider con't forget added new
+ * goto firebase > build > Authenticatiin > sigb in method
+ * Enable google sign in methode
+ * Create a button for google sign in method with a click hendelar
+ * isnide the event habdelar , call singInWithPopup with auth , provider
+ * after singInWithPopup .then result, error
+ * 
+ * 
+*  --------------------------
+*  ADDED A NEW SIGN IN METHOD
+*  --------------------------
+ * Permition this method 
+ * https://console.firebase.google.com/project/fir-authentication-d8c78/authentication/providers
+ * create a app for any one
+ * get cliend ID
+* get security id
  */
--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, GithubAuthProvider } from 'firebase/auth'
+import app from './firebase/firebase.init'
+import { useState } from 'react';
 
+
+const auth = getAuth(app)
 function App() {
+  const [users, setUsers] = useState({})
+  const {uid, displayName, } = users
+  const providerGoogle = new GoogleAuthProvider()
+  const providerGit = new GithubAuthProvider();
+
+  const handelGoogleSigninGoogle = () => {
+    signInWithPopup(auth, providerGoogle )
+      .then(result => {
+        const user = result.user;
+        setUsers(user)
+      })
+      .catch(error => {
+        console.error('error: ', error)
+      })
+  }
+
+  const handelGoogleSigninGit = () => {
+    signInWithPopup(auth, providerGit )
+      .then(result => {
+        const user = result.user;
+        setUsers(user)
+      })
+      .catch(error => {
+        console.error('error: ', error)
+      })
+  }
+
+  const handelGoogleSignOut = () => {
+
+    signOut(auth).then(() => {
+      setUsers({})
+    }).catch((error) => {
+      console.error('error: ', error)
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!uid ?
+        <>
+          <button onClick={() => handelGoogleSigninGoogle()}>sing in with google</button>
+          <button onClick={() => handelGoogleSigninGit()}>sing in with github</button>
+        </>
+        :
+        <button onClick={handelGoogleSignOut}>sing Out</button>
+      }
+      { uid && //conditonal rendaring using truthy value
+        <div>
+          <h1>User Name : {displayName}</h1>
+        </div>
+      }
+      {/* <img src={photoURL} alt='profile image' /> */}
     </div>
   );
 }
